3f5db00b0839615daeaf897f81475ccb7dc41385
AkariH <66765604+AkariH@users.noreply.github.com>
2024-04-01 03:32:10 +0000

    add command not found zsh plugin add 7z package

--- init for minecraft server.sh
+++ init for minecraft server.sh
@@ -10,7 +10,7 @@ echo "185.199.108.133 raw.githubusercontent.com" >> /etc/hosts
 apt update -y && apt upgrade -y
 
 #install common utilities
-apt install zsh tmux htop glances curl python-is-python3 -y
+apt install zsh tmux htop glances curl python-is-python3 p7zip-full -y
 
 #change default shell to zsh
 chsh -s $(which zsh)
