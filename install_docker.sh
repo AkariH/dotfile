b84b7881740f8894e9464939a1d814a57d91fd9c
AkariH <66765604+AkariH@users.noreply.github.com>
2024-08-14 06:30:44 +0000

    add docker and neovim

---
+++ install_docker.sh
@@ -0,0 +1,16 @@
+sudo apt update -y
+
+sudo apt-get -y install ca-certificates curl
+
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] http://mirrors.aliyun.com/docker-ce/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
